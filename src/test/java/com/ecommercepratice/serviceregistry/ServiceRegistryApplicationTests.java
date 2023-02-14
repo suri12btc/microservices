3664cd1f9fefa7b340b33d3dbb7397f776b7b2ad
suri12btc <123855249+suri12btc@users.noreply.github.com>
2023-02-15 00:24:54 +0530

    first
    first

--- src/test/java/com/ecommercepratice/serviceregistry/ServiceRegistryApplicationTests.java
+++ src/test/java/com/ecommercepratice/serviceregistry/ServiceRegistryApplicationTests.java
@@ -1,10 +1,10 @@
-package com.ecommerce.products;
+package com.ecommercepratice.serviceregistry;
 
 import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
 
 @SpringBootTest
-class ProductsApplicationTests {
+class ServiceRegistryApplicationTests {
 
 	@Test
 	void contextLoads() {
